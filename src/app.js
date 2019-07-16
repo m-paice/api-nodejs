d3c45ae2889727c78029ca18d3280ec2fab9dffe
Matheus Paice <matheus.ferreira@jbtec.com.br>
2019-07-16 16:12:30 -0300

    add route login

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 
 const routesUser = require("./routes/user");
 
+const User = require("./model/user");
+
 mongoose.connect(
   "mongodb://root:password@127.0.0.1:27013/example?authSource=admin",
   {
@@ -20,6 +22,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(routesUser);
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const isUser = await User.findOne({ email, password });
+
+    if (!isUser)
+      return res.status(500).json({
+        message: "Error",
+        data: "User not found"
+      });
+
+    res.status(200).json({
+      message: "OK",
+      data: isUser
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Error",
+      data: String(e)
+    });
+  }
+});
 
 app.listen(4656, function() {
   console.log("Server app listening on port 4656!");
